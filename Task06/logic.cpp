0c8e00171aab3d87ba416f5a0b05ae139706fb1f
slink <slink@DESKTOP-5P02J25>
2025-03-28 21:44:09 +0300

    Add task06

--- Task06/logic.cpp
+++ Task06/logic.cpp
@@ -7,7 +7,61 @@
 // минимальным среди всех локальных максимумов. 
 
 #include "logic.h"
+#include <iostream>
+
+
+bool is_local_max(int** matrix, int n, int m, int i, int j)
+{
+	
+
+	if (i > 0 && matrix[i - 1][j] >= matrix[i][j])
+	{
+		return false;
+	}
+	if (i < n - 1 && matrix[i + 1][j] >= matrix[i][j])
+	{
+		return false;
+	}
+	if (j > 0 && matrix[i][j - 1] >= matrix[i][j])
+	{
+		return false;
+	}
+	if (j < m - 1 && matrix[i][j + 1] >= matrix[i][j])
+	{
+		return false;
+	}
+
+
+	return true;
+}
 
 int get_min_maximum(int** matrix, int n, int m) {
-	return 0;
+
+	if (matrix == nullptr || n <= 0 || m <= 0)
+	{
+		return 0;
+	}
+
+	int minLocalMax = INT_MAX;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (is_local_max(matrix, n, m, i, j))
+			{
+				if (matrix[i][j] < minLocalMax)
+				{
+					minLocalMax = matrix[i][j];
+				}
+			}
+		}
+	}
+
+	if (n == 1 && m == 1)
+	{
+		minLocalMax = 0;
+	}
+
+	return minLocalMax;
 }
